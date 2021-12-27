8de1d631fee80d494f77b436aac319436c820948
ctownsdin <townsdin@gmail.com>
2021-12-27 14:34:54 -0800

    minor refactor and nvmrc file

--- api.js
+++ api.js
@@ -8,6 +8,34 @@ const {
 } = require("@aws-sdk/client-dynamodb");
 const { marshall, unmarshall } = require("@aws-sdk/util-dynamodb");
 
+const createPost = async (event) => {
+    const response = { statusCode: 200 };
+
+    try {
+        const body = JSON.parse(event.body);
+        const params = {
+            TableName: process.env.DYNAMODB_TABLE_NAME,
+            Item: marshall(body || {}),
+        };
+        const createResult = await db.send(new PutItemCommand(params));
+
+        response.body = JSON.stringify({
+            message: "Successfully created post.",
+            createResult,
+        });
+    } catch (e) {
+        console.error(e);
+        response.statusCode = 500;
+        response.body = JSON.stringify({
+            message: "Failed to create post.",
+            errorMsg: e.message,
+            errorStack: e.stack,
+        });
+    }
+
+    return response;
+};
+
 const getPost = async (event) => {
     const response = { statusCode: 200 };
 
@@ -29,34 +57,6 @@ const getPost = async (event) => {
         response.statusCode = 500;
         response.body = JSON.stringify({
             message: "Failed to get post.",
-            errorMsg: e.message,
-            errorStack: e.stack,
-        });
-    }
-
-    return response;
-};
-
-const createPost = async (event) => {
-    const response = { statusCode: 200 };
-
-    try {
-        const body = JSON.parse(event.body);
-        const params = {
-            TableName: process.env.DYNAMODB_TABLE_NAME,
-            Item: marshall(body || {}),
-        };
-        const createResult = await db.send(new PutItemCommand(params));
-
-        response.body = JSON.stringify({
-            message: "Successfully created post.",
-            createResult,
-        });
-    } catch (e) {
-        console.error(e);
-        response.statusCode = 500;
-        response.body = JSON.stringify({
-            message: "Failed to create post.",
             errorMsg: e.message,
             errorStack: e.stack,
         });
